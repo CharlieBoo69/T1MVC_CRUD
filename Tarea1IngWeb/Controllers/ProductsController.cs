e8278f6e3bf50e18bf3c519e7a3d057cca8e02f9
CharlieBoo69 <carlos.larco.escobar@udla.edu.ec>
2024-09-27 15:17:17 -0500

    Segundo Commit

---
+++ Tarea1IngWeb/Controllers/ProductsController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tarea1IngWeb.Models;
+
+namespace Tarea1IngWeb.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // Acción para mostrar todos los productos
+        public ActionResult Index()
+        {
+            var products = ProductRepository.GetAllProducts();
+            return View(products);
+        }
+
+        // GET: ProductsController/Details/5
+        public ActionResult Details(int id)
+        {
+            var product = ProductRepository.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // GET: ProductsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                ProductRepository.AddProduct(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: ProductsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var product = ProductRepository.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // POST: ProductsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                ProductRepository.UpdateProduct(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: ProductsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var product = ProductRepository.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // POST: ProductsController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            ProductRepository.DeleteProduct(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- Tarea1IngWeb/Models/ProductRepository.cs
+++ Tarea1IngWeb/Models/ProductRepository.cs
@@ -2,5 +2,46 @@
 {
     public class ProductRepository
     {
+        private static List<Product> _products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 1200, Quantity = 10 },
+            new Product { Id = 2, Name = "Phone", Price = 800, Quantity = 15 }
+        };
+
+        public static List<Product> GetAllProducts()
+        {
+            return _products;
+        }
+
+        public static Product GetProductById(int id)
+        {
+            return _products.FirstOrDefault(p => p.Id == id);
+        }
+
+        public static void AddProduct(Product product)
+        {
+            product.Id = _products.Max(p => p.Id) + 1;  // Genera un nuevo ID
+            _products.Add(product);
+        }
+
+        public static void UpdateProduct(Product product)
+        {
+            var existingProduct = _products.FirstOrDefault(p => p.Id == product.Id);
+            if (existingProduct != null)
+            {
+                existingProduct.Name = product.Name;
+                existingProduct.Price = product.Price;
+                existingProduct.Quantity = product.Quantity;
+            }
+        }
+
+        public static void DeleteProduct(int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product != null)
+            {
+                _products.Remove(product);
+            }
+        } 
     }
 }
